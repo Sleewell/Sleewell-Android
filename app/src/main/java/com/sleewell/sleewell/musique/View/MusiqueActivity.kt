1359462ab2bc0fe274a2448737ad68d41574c70c
gabinwdw <gabin.warnier-de-wailly@epitech.eu>
2020-08-13 14:35:57 +0900

    +add login to spotify

--- app/src/main/java/com/sleewell/sleewell/musique/View/MusiqueActivity.kt
+++ app/src/main/java/com/sleewell/sleewell/musique/View/MusiqueActivity.kt
@@ -1,12 +1,24 @@
 package com.sleewell.sleewell.musique.View
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.AdapterView
+import android.widget.Button
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sleewell.sleewell.R
 import com.sleewell.sleewell.musique.MainContract
 import com.sleewell.sleewell.musique.Presenter.MusiquePresenter
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+
 
 /**
  * This class interact with the user, display everything and catch every action of the user
@@ -17,6 +29,12 @@ class MusiqueActivity : AppCompatActivity(), MainContract.View {
 
     private lateinit var presenter: MainContract.Presenter
     private  lateinit var listView: ListView
+
+    private val clientId = "42d9f9b3f8ef4a419766c3f14566f110"
+    private val redirectUri = "http://com.sleewell.sleewell/callback"
+    private var spotifyAppRemote: SpotifyAppRemote? = null
+    private lateinit var spotify_button: Button
+
 
     companion object {
         var music_select = 0
@@ -50,6 +68,11 @@ class MusiqueActivity : AppCompatActivity(), MainContract.View {
         listView.onItemClickListener = AdapterView.OnItemClickListener{ _, _, i, _ ->
             presenter.launchMusique(i)
         }
+
+        spotify_button = findViewById(R.id.button_spotify)
+        spotify_button.setOnClickListener{
+            Launch()
+        }
     }
 
     /**
@@ -70,5 +93,38 @@ class MusiqueActivity : AppCompatActivity(), MainContract.View {
     override fun onDestroy() {
         presenter.onDestroy()
         super.onDestroy()
+    }
+
+    fun Launch() {
+        Toast.makeText(applicationContext, "Try to connect", Toast.LENGTH_LONG).show()
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
+
+
+        SpotifyAppRemote.connect(applicationContext, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Toast.makeText(applicationContext, "Fail " + throwable.message, Toast.LENGTH_LONG).show()
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        })
+
+    }
+
+    private fun connected() {
+        Toast.makeText(applicationContext, "Connected", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        SpotifyAppRemote.disconnect(spotifyAppRemote)
+        Toast.makeText(applicationContext, "Disconnected", Toast.LENGTH_LONG).show()
     }
 }
