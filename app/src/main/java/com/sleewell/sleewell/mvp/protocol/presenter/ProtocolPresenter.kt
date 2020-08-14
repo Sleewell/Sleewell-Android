29f16af5b49380c444057b0caa3dc5b99b0c52d0
Hugo <hugo.berthome@epitech.eu>
2020-08-14 17:12:13 +0900

    changing gradle version to 6.6

--- app/src/main/java/com/sleewell/sleewell/mvp/protocol/presenter/ProtocolPresenter.kt
+++ app/src/main/java/com/sleewell/sleewell/mvp/protocol/presenter/ProtocolPresenter.kt
@@ -1,2 +1,29 @@
 package com.sleewell.sleewell.mvp.protocol.presenter
 
+import androidx.appcompat.app.AppCompatActivity
+import com.sleewell.sleewell.mvp.protocol.ProtocolContract
+import com.sleewell.sleewell.networkManagement.INetworkManagement
+import com.sleewell.sleewell.networkManagement.NetworkManagement
+import org.jetbrains.annotations.Contract
+
+/**
+ * Presenter for the protocol activity
+ *
+ * @constructor Creates an instance of the presenter that link model and view and do all the logic
+ * @param view View base on the ProtocolContract.View
+ * @param ctx context is from the current activity / view
+ * @author Hugo Berthomé
+ */
+class ProtocolPresenter(view: ProtocolContract.View, ctx: AppCompatActivity) : ProtocolContract.Presenter {
+
+    private var view: ProtocolContract.View? = view
+    private val connection: INetworkManagement = NetworkManagement(ctx)
+
+    override fun onDestroy() {
+        connection.switchToSleepMode(false)
+    }
+
+    override fun onViewCreated() {
+        connection.switchToSleepMode(true)
+    }
+}
