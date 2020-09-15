fbd47e427df12e763b1b14c1adcd4f782c4c3f5d
gabinwdw <gabin.warnier-de-wailly@epitech.eu>
2020-09-15 18:38:18 +0900

    +add token

--- app/src/main/java/com/sleewell/sleewell/Spotify/View/SpotifyActivity.kt
+++ app/src/main/java/com/sleewell/sleewell/Spotify/View/SpotifyActivity.kt
@@ -27,8 +27,8 @@ class SpotifyActivity :  AppCompatActivity(), MainContract.View {
     private lateinit var accessToken: String
     private lateinit var musicSelected: SpotifyPlaylist
 
-    private val clientId = "" // /!\ need to hide
-    private val redirectUri = ""
+    private val clientId = "42d9f9b3f8ef4a419766c3f14566f110" // /!\ need to hide
+    private val redirectUri = "http://com.sleewell.sleewell/callback"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
