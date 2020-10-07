18a142585fd7a08c72af7c7a723ed1d2456f9851
gabin <gabin.warnier-de-wailly@epitech.eu>
2020-10-07 17:23:48 +0900

    +add rebase halo to master

--- app/src/main/java/com/sleewell/sleewell/mvp/protocol/ProtocolContract.kt
+++ app/src/main/java/com/sleewell/sleewell/mvp/protocol/ProtocolContract.kt
@@ -1,9 +1,59 @@
 package com.sleewell.sleewell.mvp.protocol
 
+import android.app.Dialog
+import android.graphics.ColorFilter
 import com.sleewell.sleewell.mvp.global.BasePresenter
 import com.sleewell.sleewell.mvp.global.BaseView
 
 interface ProtocolContract {
+
+    interface Model {
+        /**
+         * This method return the current size of the circle
+         *
+         * @return Int
+         * @author gabin warnier de wailly
+         */
+        fun getSizeOfCircle() : Int
+
+        /**
+         * This method return the current color of the circle
+         *
+         * @return ColorFilter
+         * @author gabin warnier de wailly
+         */
+        fun getColorOfCircle() : ColorFilter
+
+
+        /**
+         * This method reduce the size of the circle
+         *
+         * @author gabin warnier de wailly
+         */
+        fun degradesSizeOfCircle()
+
+        /**
+         * This method upgrade the size of the circle
+         *
+         * @author gabin warnier de wailly
+         */
+        fun upgradeSizeOfCircle()
+
+        /**
+         * This method reset the size of the circle
+         *
+         * @author gabin warnier de wailly
+         */
+        fun resetSizeOfCircle()
+
+        /**
+         * This method set up and return the color picker
+         *
+         * @return Dialog
+         * @author gabin warnier de wailly
+         */
+        fun openColorPicker(): Dialog
+    }
 
     interface Presenter : BasePresenter {
         /**
@@ -12,6 +62,28 @@ interface ProtocolContract {
          * @author Hugo Berthomé
          */
         fun onViewCreated()
+
+        /**
+         * this method start the protocol with a specific number of repetition
+         *
+         * @param Int the number of repetition for the halo
+         * @author gabin warnier de wailly
+         */
+        fun startProtocol(number: Int)
+
+        /**
+         * This method stop the current protocol
+         *
+         * @author gabin warnier de wailly
+         */
+        fun stopProtocol()
+
+        /**
+         * This method open the colorPicker directly on the view
+         *
+         * @author gabin warnier de wailly
+         */
+        fun openDialog()
     }
 
     interface View : BaseView<Presenter> {
@@ -22,5 +94,28 @@ interface ProtocolContract {
          * @author Hugo Berthomé
          */
         fun enableAutoLock(value: Boolean)
+
+        /**
+         * This method display the halo with the size give in param
+         *
+         * @param size size of the the halo
+         * @author gabin warnier de wailly
+         */
+        fun printHalo(size: Int)
+
+        /**
+         * This method hide the system UI for android
+         *
+         * @author gabin warnier de wailly
+         */
+        fun hideSystemUI()
+
+        /**
+         * the method set the color of the halo
+         *
+         * @param color color rgb for the halo
+         * @author gabin warnier de wailly
+         */
+        fun setColorHalo(color: ColorFilter)
     }
 }
