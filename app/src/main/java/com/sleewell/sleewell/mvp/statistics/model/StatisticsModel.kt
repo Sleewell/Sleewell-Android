12b47640aacb80c9d66719a5ddf31b82a4c9b3c6
Hugo <hugo.berthome@epitech.eu>
2020-11-07 00:15:36 +0900

    Missing file for previous commit

--- app/src/main/java/com/sleewell/sleewell/mvp/statistics/model/StatisticsModel.kt
+++ app/src/main/java/com/sleewell/sleewell/mvp/statistics/model/StatisticsModel.kt
@@ -1,4 +1,19 @@
 package com.sleewell.sleewell.mvp.statistics.model
 
-class StatisticsModel {
+import androidx.appcompat.app.AppCompatActivity
+import com.sleewell.sleewell.modules.audioRecord.IRecorderListener
+import com.sleewell.sleewell.modules.audioRecord.IRecorderManager
+import com.sleewell.sleewell.modules.audioRecord.RawRecorderManager
+import com.sleewell.sleewell.mvp.statistics.StatisticsContract
+
+class StatisticsModel(listener: IRecorderListener, context: AppCompatActivity) : StatisticsContract.Model {
+    private val recorder: IRecorderManager = RawRecorderManager(context, listener)
+
+    override fun onRecord(state: Boolean) {
+        recorder.onRecord(state)
+    }
+
+    override fun isRecording(): Boolean {
+        return recorder.isRecording()
+    }
 }
--- app/src/main/java/com/sleewell/sleewell/mvp/statistics/presenter/StatisticsPresenter.kt
+++ app/src/main/java/com/sleewell/sleewell/mvp/statistics/presenter/StatisticsPresenter.kt
@@ -1,4 +1,58 @@
 package com.sleewell.sleewell.mvp.statistics.presenter
 
-class StatisticsPresenter {
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.sleewell.sleewell.mvp.statistics.StatisticsContract
+import com.sleewell.sleewell.mvp.statistics.model.StatisticsModel
+import java.nio.Buffer
+import java.nio.ByteBuffer
+
+const val LOG_TAG = "StatisticsPresenter"
+
+class StatisticsPresenter(private var view: StatisticsContract.View, private var context: AppCompatActivity) : StatisticsContract.Presenter {
+    private val model: StatisticsContract.Model = StatisticsModel(this, context)
+
+    override fun onStartClick() {
+        model.onRecord(!model.isRecording())
+    }
+
+    /**
+     * onDestroy is called at each time e presenter will be destroyed
+     * @author Hugo Berthomé
+     */
+    override fun onDestroy() {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * When a buffer is filled, it will be sent to this callback
+     *
+     * @param buffer with audio data inside
+     * @author Hugo Berthomé
+     */
+    override fun onAudio(buffer: ByteBuffer) {
+        var array = buffer.array()
+        Log.v("OK", "OK")
+    }
+
+    /**
+     * If an error occurred, a message will be sent
+     * The record will be stopped
+     *
+     * @param message - error message
+     * @author Hugo Berthomé
+     */
+    override fun onError(message: String) {
+        view.displayToast(message)
+    }
+
+    /**
+     * On finished event is called when the recording is stopped
+     * (not called when an error occurred but onError instead)
+     *
+     * @author Hugo Berthomé
+     */
+    override fun onFinished() {
+        view.displayToast("Thread finished after 2000 milliseconds")
+    }
 }
