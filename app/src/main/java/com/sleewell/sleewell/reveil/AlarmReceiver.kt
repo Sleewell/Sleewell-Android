c4000b585a631d03bb67491645d18f1d4ac69df5
Romane <romane.bezier@epitech.eu>
2020-11-24 17:59:57 +0100

    Change volume alarm

--- app/src/main/java/com/sleewell/sleewell/reveil/AlarmReceiver.kt
+++ app/src/main/java/com/sleewell/sleewell/reveil/AlarmReceiver.kt
@@ -3,8 +3,10 @@ package com.sleewell.sleewell.reveil
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.RingtoneManager
+
 
 /**
  * Receiver of the alarm to start the notification
@@ -29,14 +31,28 @@ class AlarmReceiver : BroadcastReceiver() {
         val notificationHelper = AlarmNotificationHelper(context)
         val nb = notificationHelper.channelNotification
         notificationHelper.manager?.notify(1, nb.build())
-        var alarmUri = RingtoneManager.getActualDefaultRingtoneUri(context, RingtoneManager.TYPE_ALARM)
+        var alarmUri = RingtoneManager.getActualDefaultRingtoneUri(
+            context,
+            RingtoneManager.TYPE_ALARM
+        )
         if (alarmUri == null) {
-            alarmUri = RingtoneManager.getActualDefaultRingtoneUri(context, RingtoneManager.TYPE_NOTIFICATION)
-            if (alarmUri == null) alarmUri = RingtoneManager.getActualDefaultRingtoneUri(context, RingtoneManager.TYPE_RINGTONE)
+            alarmUri = RingtoneManager.getActualDefaultRingtoneUri(
+                context,
+                RingtoneManager.TYPE_NOTIFICATION
+            )
+            if (alarmUri == null) alarmUri = RingtoneManager.getActualDefaultRingtoneUri(
+                context,
+                RingtoneManager.TYPE_RINGTONE
+            )
         }
+        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audioManager.setStreamVolume(
+            AudioManager.STREAM_MUSIC,
+            audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) / 2,
+            0
+        )
         mp = MediaPlayer.create(context, alarmUri)
         mp.isLooping = true
         mp.start()
     }
-
 }
