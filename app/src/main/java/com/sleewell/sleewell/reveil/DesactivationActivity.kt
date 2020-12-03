ee02742ae24cc814766e0bb551eca5164d0ff866
Romane <romane.bezier@gmail.com>
2020-12-03 17:22:03 +0100

    Desactivation page

--- app/src/main/java/com/sleewell/sleewell/reveil/DesactivationActivity.kt
+++ app/src/main/java/com/sleewell/sleewell/reveil/DesactivationActivity.kt
@@ -3,17 +3,24 @@ package com.sleewell.sleewell.reveil
 import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sleewell.sleewell.R
 import kotlinx.android.synthetic.main.activity_desactivation_alarm.*
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class DesactivationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_desactivation_alarm)
+
+        val currentDateTime = LocalDateTime.now()
+        val time = currentDateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
+
+        time_desactivation.text = time
 
         val stopIntent = Intent(applicationContext, GlobalReceiver::class.java).apply {
             action = "Stop"
@@ -22,6 +29,16 @@ class DesactivationActivity : AppCompatActivity() {
 
         swipe_button.setOnStateChangeListener {
             stopPendingIntent.send()
+            finish()
+        }
+
+        val snoozeIntent = Intent(applicationContext, GlobalReceiver::class.java).apply {
+            action = "Snooze"
+        }
+        val snoozePendingIntent = PendingIntent.getBroadcast(applicationContext, 1, snoozeIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        snooze_desactivation.setOnClickListener {
+            snoozePendingIntent.send()
             finish()
         }
     }
