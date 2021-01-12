ea5d961674cff2968232e9b1984e973141791a35
Hugo <hugo.berthome@epitech.eu>
2021-01-12 15:34:56 +0900

    Adapting Instrumental tests with new UI

--- app/src/main/java/com/sleewell/sleewell/mvp/mainActivity/view/MainActivity.kt
+++ app/src/main/java/com/sleewell/sleewell/mvp/mainActivity/view/MainActivity.kt
@@ -3,10 +3,26 @@ package com.sleewell.sleewell.mvp.mainActivity.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sleewell.sleewell.R
+import com.sleewell.sleewell.mvp.mainActivity.MainContract
+import com.sleewell.sleewell.mvp.mainActivity.presenter.MainPresenter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
+    private lateinit var presenter : MainContract.Presenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_activity_main)
+        setPresenter(MainPresenter(this, this))
+        presenter.onViewCreated()
+    }
+
+    /**
+     * Set the presenter inside the class
+     *
+     * @param presenter
+     * @author Hugo Berthomé
+     */
+    override fun setPresenter(presenter: MainContract.Presenter) {
+        this.presenter = presenter
     }
 }
