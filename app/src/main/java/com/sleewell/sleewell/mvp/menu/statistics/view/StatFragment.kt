9c72bf33f94881a2167f6ecf72f4649cf83588a9
Hugo <hugo.berthome@epitech.eu>
2021-01-13 11:22:52 +0900

    Remome useless companion object

--- app/src/main/java/com/sleewell/sleewell/mvp/menu/statistics/view/StatFragment.kt
+++ app/src/main/java/com/sleewell/sleewell/mvp/menu/statistics/view/StatFragment.kt
@@ -17,21 +17,7 @@ import com.sleewell.sleewell.mvp.menu.statistics.StatisticsContract
 import com.sleewell.sleewell.mvp.menu.statistics.presenter.StatisticsPresenter
 import com.sleewell.sleewell.mvp.menu.statistics.model.AnalyseValueStatistic
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StatFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StatFragment : Fragment(), StatisticsContract.View {
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var presenter: StatisticsContract.Presenter
     private lateinit var root: View
@@ -44,14 +30,6 @@ class StatFragment : Fragment(), StatisticsContract.View {
     private lateinit var textView: TextView
     private lateinit var loadingProgressBar: ProgressBar
     private lateinit var errorIcon: ImageView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -80,26 +58,6 @@ class StatFragment : Fragment(), StatisticsContract.View {
         errorIcon.visibility = View.INVISIBLE
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StatFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-
     /**
      * Update the graph from the analyse data
      *
@@ -123,12 +81,12 @@ class StatFragment : Fragment(), StatisticsContract.View {
             .animationType(AAChartAnimationType.EaseInOutSine)
             .animationDuration(1000)
             .categories(
-                Array(datas.size) { i ->  datas[i].ts }
+                Array(datas.size) { i -> datas[i].ts }
             )
             .series(
                 arrayOf(
                     AASeriesElement().data(
-                        Array(datas.size) { i ->  datas[i].db }
+                        Array(datas.size) { i -> datas[i].db }
                     )
                 )
             )
