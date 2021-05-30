7e6566012663ea30164c72d84994a31b53d46242
Hugo <hugo.berthome@epitech.eu>
2021-05-30 15:44:42 +0900

    Open activity from setting preference fragment

---
+++ app/src/main/java/com/sleewell/sleewell/mvp/help/HelpSignalmenActivity.kt
@@ -0,0 +1,12 @@
+package com.sleewell.sleewell.mvp.help
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sleewell.sleewell.R
+
+class HelpSignalmenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help_signalement)
+    }
+}
