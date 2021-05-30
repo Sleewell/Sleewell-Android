6ffc60ee00fb2a46834b8900a504b9f82ced433b
Hugo <hugo.berthome@epitech.eu>
2021-05-30 16:50:44 +0900

    Create help layout with report button

--- app/src/main/java/com/sleewell/sleewell/mvp/help/HelpSignalmenActivity.kt
+++ app/src/main/java/com/sleewell/sleewell/mvp/help/HelpSignalmenActivity.kt
@@ -1,12 +1,39 @@
 package com.sleewell.sleewell.mvp.help
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.sleewell.sleewell.BuildConfig
 import com.sleewell.sleewell.R
+
 
 class HelpSignalmenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help_signalement)
+
+        val versionCode = BuildConfig.VERSION_CODE
+        val versionName = BuildConfig.VERSION_NAME
+        val versionText = findViewById<TextView>(R.id.textVersion)
+        versionText.text = getString(R.string.version_app, versionName, versionCode.toString())
+
+        val buttonSignal = findViewById<Button>(R.id.buttonProblemSignal)
+        buttonSignal.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://sleewell.fr/?version=$versionName-$versionCode")
+                )
+            )
+        }
+
+        val buttonReturn = findViewById<ImageButton>(R.id.imageReturnHelp)
+        buttonReturn.setOnClickListener {
+            finish()
+        }
     }
 }
