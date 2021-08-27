150f8a787da374bb69014510b19d551fc4528827
Titouan FIANCETTE <titouan.fiancette@epitech.eu>
2021-08-27 23:59:31 +0200

    Changing theme and customising alert dialog

--- app/src/main/java/com/sleewell/sleewell/mvp/menu/profile/view/PickImageDialog.kt
+++ app/src/main/java/com/sleewell/sleewell/mvp/menu/profile/view/PickImageDialog.kt
@@ -1,11 +1,11 @@
 package com.sleewell.sleewell.mvp.menu.profile.view
 
-import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
 
 class PickImageDialog : DialogFragment() {
     // Use this instance of the interface to deliver action events
@@ -19,24 +19,24 @@ class PickImageDialog : DialogFragment() {
         fun onDialogPickPictureClick(dialog: DialogFragment)
     }
 
-    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    // Override the Fragment.onAttach() method to instantiate the DialogEventListener
     override fun onAttach(context: Context) {
         super.onAttach(context)
         // Verify that the host activity implements the callback interface
         try {
-            // Instantiate the NoticeDialogListener so we can send events to the host
+            // Instantiate the DialogEventListener so we can send events to the host
             eventListener = context as DialogEventListener
         } catch (e: ClassCastException) {
             // The activity doesn't implement the interface, throw exception
             throw ClassCastException((context.toString() +
-                    " must implement NoticeDialogListener"))
+                    " must implement DialogEventListener"))
         }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity.let {
-            AlertDialog.Builder(requireContext())
-                .setItems( arrayOf(
+            MaterialAlertDialogBuilder(requireContext())
+                .setItems(arrayOf(
                     "Take a picture",
                     "Pick from gallery"))
                 { dialog, which ->
@@ -48,5 +48,4 @@ class PickImageDialog : DialogFragment() {
                 }.create()
         }
     }
-
 }
